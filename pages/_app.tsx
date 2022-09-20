6e262a61bf9ce14d204b009921d197ca52f51a4f
Andres <andres.porfirio@gmail.com>
2022-09-20 16:17:31 -0300

    added antd

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,7 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+
+import '../styles/globals.css'
+import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
