2eb86c71751ecdbd04a88bbfb9871ad7092bb9e3
ricardodidimo <ricardodidimo21@gmail.com>
2022-09-23 00:39:19 -0300

    Add tests for 'BcryptHasher.Hash' method

---
+++ utils/hasher/__tests/bcryptjs/HashAsync.test.ts
@@ -0,0 +1,19 @@
+import bcryptHasher from "../../impl/BcryptjsHasher";
+import HasherError from '../../errors/HasherError';
+import Hasher, {HashResponse } from '../../interfaces/Hasher';
+
+test('HashAsync_OnValidInput_ReturnsTransformedOutput', async () => {
+  // Arrange
+  const hasher: Hasher = new bcryptHasher();
+  const inputTest = "p@ssw0rd";
+
+  // Act
+  const response: HasherError | HashResponse = await hasher.hashAsync(inputTest);
+  
+  // Assert
+  const isSuccessfulResponse: Boolean = (response as HashResponse).hashedInput != undefined;
+  expect(isSuccessfulResponse).toBeTruthy;
+
+  const doOutputStringDiffersFromInput: Boolean = (response as HashResponse).hashedInput != inputTest;
+  expect(doOutputStringDiffersFromInput).toBeTruthy();
+});
