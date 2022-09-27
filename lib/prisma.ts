2c20210102b6b70144e8e40030d1c205a0cacdca
ricardodidimo <ricardodidimo21@gmail.com>
2022-09-27 14:21:30 -0300

    Change 'prisma.ts' to declare global prisma instance

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,14 +1,13 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
 
-let prisma: PrismaClient;
-
-if (process.env.NODE_ENV === 'production') {
-  prisma = new PrismaClient();
-} else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient();
-  }
-  prisma = global.prisma;
+declare global {
+  var prisma: PrismaClient | undefined;
 }
 
-export default prisma;
+export const prisma =
+  global.prisma ||
+  new PrismaClient({
+    log: ["query"],
+  });
+
+if (process.env.NODE_ENV !== "production") global.prisma = prisma;
