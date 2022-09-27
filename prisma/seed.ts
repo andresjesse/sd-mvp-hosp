104a29f88fb1db0c4bad4f261910c8355fc32c27
ricardodidimo <ricardodidimo21@gmail.com>
2022-09-27 16:40:09 -0300

    Rollback rename of admin seed file

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,6 +1,6 @@
 import { sectors } from "./seeds/sectors";
 import {prisma} from "../lib/prisma";
-import AdminSeed from "./seeders/admin.seed";
+import AdminSeed from "./seeders/admin";
 
 async function main() {
   await prisma.sector.createMany({
