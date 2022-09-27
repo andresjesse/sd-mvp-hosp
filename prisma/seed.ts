9a9f633883ef36112f29352e33aa8b20c9cfa91b
ricardodidimo <ricardodidimo21@gmail.com>
2022-09-27 14:55:35 -0300

    Add a central seeding function on 'seed.ts'

---
+++ prisma/seed.ts
@@ -0,0 +1,16 @@
+import {prisma} from "../lib/prisma";
+import AdminSeed from "./seeders/admin";
+
+async function main() {
+  await AdminSeed();
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
