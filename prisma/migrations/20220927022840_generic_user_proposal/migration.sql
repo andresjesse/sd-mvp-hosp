10c72124519dbef1012d8806109847d315d15201
ricardodidimo <ricardodidimo21@gmail.com>
2022-09-26 23:55:26 -0300

    Change prisma schema; add '@unique" constraint to email column

--- prisma/migrations/20220927022840_generic_user_proposal/migration.sql
+++ prisma/migrations/20220927022840_generic_user_proposal/migration.sql
@@ -8,6 +8,7 @@ CREATE TABLE `User` (
     `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updated_at` DATETIME(3) NOT NULL,
 
+    UNIQUE INDEX `User_email_key`(`email`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
