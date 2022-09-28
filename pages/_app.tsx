83fc67f25984efe1a3cdf256d2e979c809ee6c78
MateusVinicio <mateusvinicio1@gmail.com>
2022-09-28 16:22:22 -0300

    Add global layout and menu

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,15 @@
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import Layout from "../components/Layout";
 
-import '../styles/globals.css'
-import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
+import "../styles/globals.css";
+import "antd/dist/antd.css"; // or 'antd/dist/antd.less'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
 
-export default MyApp
+export default MyApp;
