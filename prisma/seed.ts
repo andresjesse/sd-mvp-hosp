4b9fae3347fba95f0c81e0bd401922c6a05f102a
Ricardo Didimo <86539214+ricardodidimo@users.noreply.github.com>
2022-09-29 16:24:06 -0300

    Generate admin seed script (#4)
    * Add '@prisma/client' as a dependency
    
    * Create a prisma instance manager
    
    * Add proposal for prisma schema
    
    * Add migrations generated by schema proposal
    
    * Add 'Generate' script for database seeding and admin creation
    
    * Add 'ts-node', '@types/node' as devDependencies
    
    This dependencies are recommended from prisma docs for
    'database seeding'
    https://www.prisma.io/docs/guides/database/seed-database
    
    * Add 'prisma.seed' command to package.json
    
    * Change 'Generate.ts' script to 'Admin.ts'
    
    This moves the seed logic to prisma folder and also relies
    on admin info coming from .env file instead of hardcoded strings.
    
    * Add 'admin generation' related variables to .env.example
    
    * Change prisma schema; add '@unique" constraint to email column
    
    * Change 'prisma.seed' value on package.json to "placeholder" state
    
    * Move 'prisma.ts' config file to 'lib' folder at root
    
    * Change 'Admin.ts' file to depend on '...lib/prisma'
    
    * Change 'prisma.ts' to declare global prisma instance
    
    * Update prisma import on 'Admin.ts'
    
    * Add export default to 'Admin.ts'
    
    * Add a central seeding function on 'seed.ts'
    
    * Add 'seed' prisma command to package.json
    
    * Rename 'admin.ts' seed script to 'admin.seed.ts'
    
    * Update admin seed imports
    
    * Rollback rename of admin seed file
    
    * Prettier lint changes
    
    * renamed 'seeders' folder to 'seeds'
    
    * added 'TAdminUser' object type as return type for seed function
    
    * changed 'seed.ts' to expected 'AdminSeed' to return an object
    
    * added referential integrity constraints to 'User' dependent tables
    
    * removed 'yarnrc' file from gitignore; updated yarn related gitignore rules
    
    * Create a prisma instance manager
    
    * Add proposal for prisma schema
    
    * Add migrations generated by schema proposal
    
    * Add 'Generate' script for database seeding and admin creation
    
    * Change 'Generate.ts' script to 'Admin.ts'
    
    This moves the seed logic to prisma folder and also relies
    on admin info coming from .env file instead of hardcoded strings.
    
    * Add 'admin generation' related variables to .env.example
    
    * Change prisma schema; add '@unique" constraint to email column
    
    * Move 'prisma.ts' config file to 'lib' folder at root
    
    * Change 'Admin.ts' file to depend on '...lib/prisma'
    
    * Update prisma import on 'Admin.ts'
    
    * Add export default to 'Admin.ts'
    
    * Add a central seeding function on 'seed.ts'
    
    * Rename 'admin.ts' seed script to 'admin.seed.ts'
    
    * Update admin seed imports
    
    * Rollback rename of admin seed file
    
    * renamed 'seeders' folder to 'seeds'
    
    * added 'TAdminUser' object type as return type for seed function
    
    * changed 'seed.ts' to expected 'AdminSeed' to return an object
    
    * added referential integrity constraints to 'User' dependent tables
    
    * removed 'yarnrc' file from gitignore; updated yarn related gitignore rules
    
    * added re-generated migration
    
    * removed old migration
    
    * added seeding calls to transactional mode
    
    * added prettier style changes
    
    * removed comments from 'lib/prisma'
    
    Co-authored-by: ricardodidimo <ricardodidimo21@gmail.com>

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,10 +1,18 @@
 import { sectors } from "./seeds/sectors";
 import { prisma } from "../lib/prisma";
+import AdminSeedFunction from "./seeds/admin";
 
 async function main() {
-  await prisma.sector.createMany({
-    data: sectors,
-  });
+  const adminSeedData = await AdminSeedFunction();
+
+  const [sectorsResult, adminResult] = await prisma.$transaction([
+    prisma.sector.createMany({
+      data: sectors,
+    }),
+    prisma.user.create({
+      data: adminSeedData,
+    }),
+  ]);
 }
 
 main()
