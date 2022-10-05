71a644b5feaf51aceecac809ccc90cbc40cb6ded
andresjesse <andres.porfirio@gmail.com>
2022-10-05 19:21:10 -0300

    Doctor login forms (#7)
    * create new branch doctor forms by alguipires
    
    * inicializado pag. cadastro e login
    
    * cleaned and fixed responsive design in user/new form
    
    * updated form style
    
    * moved login to root
    
    * updated login fields
    
    Co-authored-by: alguipires <alguipires@gmail.com>

---
+++ pages/login.tsx
@@ -0,0 +1,69 @@
+import { Button, Card, Checkbox, Form, Input } from "antd";
+import Link from "next/link";
+import React from "react";
+
+const App: React.FC = () => {
+  const onFinish = (values: any) => {
+    console.log("Success:", values);
+  };
+
+  const onFinishFailed = (errorInfo: any) => {
+    console.log("Failed:", errorInfo);
+  };
+
+  return (
+    <Card>
+      <Form
+        name="basic"
+        labelCol={{ span: 8 }}
+        wrapperCol={{ span: 16 }}
+        initialValues={{ remember: true }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete="off"
+      >
+        <Form.Item
+          label="Email"
+          name="email"
+          rules={[
+            {
+              required: true,
+              message: "Insira um email válido!",
+              type: "email",
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          label="Senha"
+          name="password"
+          rules={[{ required: true, message: "Preencha sua senha!" }]}
+        >
+          <Input.Password />
+        </Form.Item>
+
+        {/* <Form.Item
+          name="remember"
+          valuePropName="checked"
+          wrapperCol={{ offset: 8, span: 16 }}
+        >
+          <Checkbox>Lembrar</Checkbox>
+        </Form.Item>
+
+        <Form.Item name="forgot" wrapperCol={{ offset: 8, span: 16 }}>
+          <Link href="/">Esquecia a senha</Link>
+        </Form.Item> */}
+
+        <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+          <Button type="primary" htmlType="submit">
+            Login
+          </Button>
+        </Form.Item>
+      </Form>
+    </Card>
+  );
+};
+
+export default App;
---
+++ pages/user/new.tsx
@@ -0,0 +1,109 @@
+import {
+  Button,
+  DatePicker,
+  Form,
+  Input,
+  Select,
+  Col,
+  Row,
+  Divider,
+  Typography,
+  Card,
+} from "antd";
+
+import React, { useState } from "react";
+const { Title } = Typography;
+type SizeType = Parameters<typeof Form>[0]["size"];
+
+{
+  /**import enUS from 'antd/es/locale/en_US';
+import moment from 'moment';
+import 'moment/locale/zh-cn';
+
+moment.locale('en'); */
+}
+
+{
+  /**FAZER***
+RESPONSIVO.
+LINGUAGEM DO ELEMENTO DATAPICKER EM PORTUGUES.
+IDENTAR CODE.
+ */
+}
+
+const App: React.FC = () => {
+  return (
+    <Card>
+      <Row justify="center">
+        <Title level={3}>Cadastro Novo usuario</Title>
+      </Row>
+
+      <Row justify="center" style={{ padding: "5%" }}>
+        <Form
+          layout="horizontal"
+          labelWrap
+          labelCol={{ flex: "120px" }}
+          wrapperCol={{ flex: 1 }}
+          colon={false}
+          style={{ width: "100%" }}
+        >
+          <Form.Item label="Nome">
+            <Input name="nameDoctor" />
+          </Form.Item>
+
+          <Form.Item label="Sobrenome">
+            <Input name="lastNameDoctor" />
+          </Form.Item>
+
+          {/* => Pode remover isso, a princípio não vamos usar.
+        <Form.Item label="Função: ">
+          <Select>
+            <Select.Option value="doctor">Medico(a)</Select.Option>
+            <Select.Option value="enf">Enfermeiro(a)</Select.Option>
+          </Select>
+        </Form.Item>
+        */}
+
+          <Form.Item label="Data de Nascimento: ">
+            <DatePicker />
+          </Form.Item>
+
+          <Form.Item label="CRM: ">
+            <Input name="crm" />
+          </Form.Item>
+
+          <Form.Item label="CRM UF: ">
+            <Select>
+              <Select.Option value="pr">Paraná</Select.Option>
+              <Select.Option value="sp">São Paulo</Select.Option>
+            </Select>
+          </Form.Item>
+
+          <Form.Item label="Email: ">
+            <Input name="email" alt="Seu E-mail" />
+          </Form.Item>
+
+          <Form.Item label="Confirme o Email: ">
+            <Input name="confEmail" alt="Confirme seu E-mail" />
+          </Form.Item>
+
+          <Form.Item label="Senha: ">
+            <Input name="password" alt="Senha" />
+          </Form.Item>
+
+          <Form.Item label="Confirme sua Senha: ">
+            <Input name="confPassword" alt="Confirme a Senha" />
+          </Form.Item>
+
+          <Form.Item label=" ">
+            <Button type="primary" htmlType="submit">
+              Cadastrar
+            </Button>
+          </Form.Item>
+        </Form>
+      </Row>
+    </Card>
+  );
+};
+
+export default App;
