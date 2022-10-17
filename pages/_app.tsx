b9924d7995c9ebc58284eac2f249a0e0d33084ae
Andres <andres.porfirio@gmail.com>
2022-10-17 11:47:15 -0300

    initial ConfigProvider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,38 @@
+// import type { AppProps } from "next/app";
+// import Layout from "../components/Layout";
+
+// import "../styles/globals.css";
+// import "antd/dist/antd.css"; // or 'antd/dist/antd.less'
+
+// function MyApp({ Component, pageProps }: AppProps) {
+//   return (
+//     <Layout>
+//       <Component {...pageProps} />
+//     </Layout>
+//   );
+// }
+
+// export default MyApp;
 import type { AppProps } from "next/app";
 import Layout from "../components/Layout";
+import { ConfigProvider } from "antd";
 
 import "../styles/globals.css";
-import "antd/dist/antd.css"; // or 'antd/dist/antd.less'
+import "antd/dist/antd.variable.min.css";
+
+ConfigProvider.config({
+  theme: {
+    primaryColor: "#3e9278",
+  },
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <ConfigProvider>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </ConfigProvider>
   );
 }
 
