e9d302f938c414971d1c3e4e0285698ef2996e3d
andresjesse <andres.porfirio@gmail.com>
2022-10-17 16:09:39 -0300

    fixes pre-merge

--- pages/api/auth/signin/invalidCredentials.ts
+++ pages/api/auth/signin/invalidCredentials.ts
@@ -1,12 +1,5 @@
 export class InvalidCredentials extends Error {
   constructor() {
-    const message: string = "Invalid credentials.";
-    super(message);
-
-    Object.setPrototypeOf(this, InvalidCredentials.prototype);
-  }
-
-  getErrorMessage() {
-    return this.message;
+    super("Invalid credentials.");
   }
 }
