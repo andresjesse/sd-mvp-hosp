34edc98eab1f66c8582bd1ef08c187d37a5d19c4
ricardodidimo <ricardodidimo21@gmail.com>
2022-10-18 20:05:14 -0300

    updated 'TSessionUser' imports on 'middleware'

--- middleware.ts
+++ middleware.ts
@@ -1,33 +1,33 @@
-import { Role } from "@prisma/client";
-import { withAuth } from "next-auth/middleware";
-import { TSessionUser } from "./pages/api/auth/signin/authorize";
+import { Role } from '@prisma/client'
+import { withAuth } from 'next-auth/middleware'
+import { TSessionUser } from './pages/api/auth/[...nextauth]'
 
 /* This follows as same as the declaration on 
 https://next-auth.js.org/configuration/nextjs#middleware */
 export default withAuth(
   /* This is only called when 'authorized' callback returns true */
   function middleware(req) {
-    const user = req.nextauth.token?.user as TSessionUser;
-    console.log(user);
+    const user = req.nextauth.token?.user as TSessionUser
+    console.log(user)
   },
   {
     callbacks: {
       authorized: ({ token }) => {
-        const isNotAuthenticated: Boolean = token === null;
+        const isNotAuthenticated: boolean = token === null
         if (isNotAuthenticated) {
-          return false;
+          return false
         }
 
-        const user: TSessionUser = token?.user as TSessionUser;
-        console.log("is Admin?");
-        console.log(user.role === Role.ADMIN);
-        return true;
+        const user: TSessionUser = token?.user as TSessionUser
+        console.log('is Admin?')
+        console.log(user.role === Role.ADMIN)
+        return true
       },
     },
     pages: {
-      signIn: "/login",
+      signIn: '/login',
     },
   }
-);
+)
 
-export const config = { matcher: ["/welcome"] };
+export const config = { matcher: ['/welcome'] }
