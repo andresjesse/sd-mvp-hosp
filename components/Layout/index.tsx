0ed8c4e3eaecda25c6606b02eb26c07b373dbf89
MateusVinicio <32345776+MateusVinicio@users.noreply.github.com>
2022-10-20 15:50:13 -0300

    @mateus/feat theme vars (#12)
    * initial ConfigProvider
    
    * add links sider
    
    * updated styles
    
    * updated styles. added conditional rendering for logged users
    
    * added logo
    
    * content margins and padding adjust
    
    Co-authored-by: Andres <andres.porfirio@gmail.com>

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -1,69 +1,131 @@
 import {
   FieldTimeOutlined,
+  LoginOutlined,
   LogoutOutlined,
   UserOutlined,
-} from "@ant-design/icons";
-import { Layout, Menu } from "antd";
-import { ReactNode, useState } from "react";
+} from '@ant-design/icons'
+import { Button, Layout, Menu } from 'antd'
+import { signOut, useSession } from 'next-auth/react'
+import Image from 'next/image'
+import Link from 'next/link'
+import { ReactNode, useState } from 'react'
 
-const { Header, Content, Sider, Footer } = Layout;
+const { Header, Content, Sider, Footer } = Layout
 
 interface PageLayoutProps {
-  children: ReactNode;
+  children: ReactNode
 }
 
 const siderItems = [
   {
-    label: "Médico",
-    key: "doctor",
-    icon: <UserOutlined style={{ color: "black" }} />,
+    label: (
+      <Link href="/profile">
+        <a>Médico</a>
+      </Link>
+    ),
+    key: 'doctor',
+    icon: <UserOutlined />,
   },
   {
-    label: "Escala",
-    key: "schedule",
-    icon: <FieldTimeOutlined style={{ color: "black" }} />,
+    label: (
+      <Link href="/schedule">
+        <a>Escala</a>
+      </Link>
+    ),
+    key: 'schedule',
+    icon: <FieldTimeOutlined />,
   },
-];
+]
 
 export default function PageLayout({ children }: PageLayoutProps) {
-  const [collapsed, setCollapsed] = useState(false);
+  const { data: session, status } = useSession()
+  const [collapsed, setCollapsed] = useState(false)
+
   return (
     <Layout>
       <Header
         className="header"
         style={{
-          paddingLeft: "24px",
-          paddingRight: "24px",
-          color: "white !important",
-          display: "flex",
-          justifyContent: "space-between",
+          paddingLeft: '24px',
+          paddingRight: '24px',
+          color: 'white !important',
+          display: 'flex',
+          justifyContent: 'space-between',
         }}
       >
-        <div>LOGO AQUI</div>
-        <div>
-          Logout <LogoutOutlined />
-        </div>
+        <Link href="/">
+          <div style={{ display: 'flex', cursor: 'pointer' }}>
+            <div
+              style={{
+                position: 'relative',
+                width: '56px',
+              }}
+            >
+              <Image
+                alt="Image Alt"
+                src="/logo.png"
+                layout="fill"
+                objectFit="contain" // Scale your image down to fit into the container
+              />
+            </div>
+            <span style={{ marginLeft: 10, fontWeight: 'bold' }}>MVP SD</span>
+          </div>
+        </Link>
+
+        {status === 'authenticated' ? (
+          <div
+            style={{
+              display: 'flex',
+              alignItems: 'center',
+              whiteSpace: 'nowrap',
+            }}
+          >
+            Logado como {session?.user?.name}
+            <Button
+              type="text"
+              onClick={() => signOut({ callbackUrl: '/' })}
+              style={{ fontWeight: 'bold' }}
+            >
+              Deslogar-se
+              <LogoutOutlined />
+            </Button>
+          </div>
+        ) : (
+          <Link href="/login">
+            <div style={{ fontWeight: 'bold' }}>
+              <span style={{ marginRight: 10, cursor: 'pointer' }}>
+                Fazer Login
+              </span>
+              <LoginOutlined />
+            </div>
+          </Link>
+        )}
       </Header>
 
-      <Layout style={{ minHeight: "100vh" }}>
-        <Sider
-          collapsible
-          collapsed={collapsed}
-          onCollapse={(value) => setCollapsed(value)}
-        >
-          <Menu mode="inline" defaultSelectedKeys={["1"]} items={siderItems} />
-        </Sider>
+      <Layout style={{ minHeight: '100vh' }}>
+        {status === 'authenticated' && (
+          <Sider
+            collapsible
+            collapsed={collapsed}
+            onCollapse={(value) => setCollapsed(value)}
+          >
+            <Menu
+              mode="inline"
+              defaultSelectedKeys={['1']}
+              items={siderItems}
+              theme="dark"
+            />
+          </Sider>
+        )}
 
-        <Layout style={{ padding: "0 24px 24px" }}>
-          <Content style={{ padding: 24, margin: 0, minHeight: 280 }}>
-            {children}
-          </Content>
+        <Layout>
+          <Content style={{ padding: 10, minHeight: 280 }}>{children}</Content>
 
-          <Footer style={{ textAlign: "center" }}>
+          <Footer style={{ textAlign: 'center' }}>
             Hospital MVP ©2022 Created by TSI
           </Footer>
         </Layout>
       </Layout>
     </Layout>
-  );
+  )
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,24 +1,48 @@
-import type { AppProps } from "next/app";
-import Layout from "../components/Layout";
-import { SessionProvider } from "next-auth/react";
-import { Session } from "next-auth";
+// import type { AppProps } from "next/app";
+// import Layout from "../components/Layout";
 
-import "../styles/globals.css";
-import "antd/dist/antd.css"; // or 'antd/dist/antd.less'
+// import "../styles/globals.css";
+// import "antd/dist/antd.css"; // or 'antd/dist/antd.less'
+
+// function MyApp({ Component, pageProps }: AppProps) {
+//   return (
+//     <Layout>
+//       <Component {...pageProps} />
+//     </Layout>
+//   );
+// }
+
+// export default MyApp;
+import type { AppProps } from 'next/app'
+import Layout from '../components/Layout'
+import { ConfigProvider } from 'antd'
+import { SessionProvider } from 'next-auth/react'
+import { Session } from 'next-auth'
+
+import '../styles/globals.css'
+import 'antd/dist/antd.variable.min.css'
+
+ConfigProvider.config({
+  theme: {
+    primaryColor: '#3e9278',
+  },
+})
 
 function MyApp({
   Component,
   pageProps,
 }: AppProps<{
-  session: Session;
+  session: Session
 }>) {
   return (
     <SessionProvider session={pageProps.session}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <ConfigProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ConfigProvider>
     </SessionProvider>
-  );
+  )
 }
 
-export default MyApp;
+export default MyApp
