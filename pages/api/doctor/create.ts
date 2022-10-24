2de9b11a01fa45a781363dcf8fb00d060e0edb25
ricardodidimo <ricardodidimo21@gmail.com>
2022-10-24 20:27:49 -0300

    changed 'create' route to use global error handler

--- pages/api/doctor/create.ts
+++ pages/api/doctor/create.ts
@@ -2,39 +2,36 @@
 import { Doctor } from '@prisma/client'
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { prisma } from '../../../lib/prisma'
+import withErrorHandler from '../../../utils/api/withErrorHandler'
 import hasher from '../../../utils/hasher/BcryptjsHasher'
 
-export default async function handler(
+const handlerFunction = async (
   req: NextApiRequest,
   res: NextApiResponse<Doctor | null>
-) {
+) => {
   if (req.method === 'POST') {
-    try {
-      console.log(req.body)
+    console.log(req.body)
 
-      const { name, email, password, crm, crmUf } = req.body
+    const { name, email, password, crm, crmUf } = req.body
 
-      const passwordHash = await hasher.hashAsync(password)
+    const passwordHash = await hasher.hashAsync(password)
 
-      const doctor = await prisma.doctor.create({
-        data: {
-          crm,
-          crmUf,
-          user: {
-            create: {
-              name,
-              email,
-              passwordHash,
-            },
+    const doctor = await prisma.doctor.create({
+      data: {
+        crm,
+        crmUf,
+        user: {
+          create: {
+            name,
+            email,
+            passwordHash,
           },
         },
-      })
+      },
+    })
 
-      res.status(201).json(doctor)
-    } catch (error) {
-      //TODO: migate to error handler
-      console.log(error)
-      res.status(500).json(null)
-    }
+    res.status(201).json(doctor)
   }
 }
+
+export default withErrorHandler(handlerFunction)
