81241e31120761e871cf85262edf6a873546ed05
Ricardo Didimo <86539214+ricardodidimo@users.noreply.github.com>
2022-10-31 19:45:37 -0300

    @ricardo/error handler (#16)
    * added global API error handler
    
    * changed 'create' route to use global error handler
    
    * updated pr
    
    * fixed eslint error
    
    Co-authored-by: ricardodidimo <ricardodidimo21@gmail.com>
    Co-authored-by: andresjesse <andres.porfirio@gmail.com>

--- pages/api/doctor/create.ts
+++ pages/api/doctor/create.ts
@@ -2,39 +2,43 @@
 import { Doctor } from '@prisma/client'
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { prisma } from '../../../lib/prisma'
+import { ApiHandleError } from '../../../utils/api/apiHandleError'
+import withErrorHandler from '../../../utils/api/withErrorHandler'
 import hasher from '../../../utils/hasher/BcryptjsHasher'
 
-export default async function handler(
+const handlerFunction = async (
   req: NextApiRequest,
   res: NextApiResponse<Doctor | null>
-) {
+) => {
   if (req.method === 'POST') {
-    try {
-      console.log(req.body)
+    const { name, email, password, crm, crmUf } = req.body
 
-      const { name, email, password, crm, crmUf } = req.body
+    const errors: { [key: string]: string | Iterable<string> } = {}
+    if (!name) errors['name'] = ["Name can't be empty!"]
+    if (!email) errors['email'] = ["Email can't be empty!"]
+    if (!password) errors['password'] = ["Password can't be empty!"]
+    if (!crm) errors['crm'] = ["CRM can't be empty!"]
+    if (!crmUf) errors['crmUf'] = ["CRM UF can't be empty!"]
+    if (Object.keys(errors).length > 0) throw new ApiHandleError(400, errors)
 
-      const passwordHash = await hasher.hashAsync(password)
+    const passwordHash = await hasher.hashAsync(password)
 
-      const doctor = await prisma.doctor.create({
-        data: {
-          crm,
-          crmUf,
-          user: {
-            create: {
-              name,
-              email,
-              passwordHash,
-            },
+    const doctor = await prisma.doctor.create({
+      data: {
+        crm,
+        crmUf,
+        user: {
+          create: {
+            name,
+            email,
+            passwordHash,
           },
         },
-      })
+      },
+    })
 
-      res.status(201).json(doctor)
-    } catch (error) {
-      //TODO: migate to error handler
-      console.log(error)
-      res.status(500).json(null)
-    }
+    res.status(201).json(doctor)
   }
 }
+
+export default withErrorHandler(handlerFunction)
---
+++ utils/api/withErrorHandler.ts
@@ -0,0 +1,30 @@
+import { NextApiHandler, NextApiRequest, NextApiResponse } from 'next'
+import { ApiHandleError } from './apiHandleError'
+
+export default function withErrorHandler(
+  handler: NextApiHandler
+): NextApiHandler {
+  return async (req: NextApiRequest, res: NextApiResponse) => {
+    try {
+      return await handler(req, res)
+    } catch (error) {
+      const isApiError = error instanceof ApiHandleError
+
+      if (process.env.NODE_ENV === 'development') {
+        console.log(error)
+      }
+
+      if (!isApiError) {
+        res.status(500).json({
+          data: 'Something went wrong on server side. Try again or contact us.',
+        })
+        return
+      }
+
+      const apiError: ApiHandleError = error as ApiHandleError
+      res.status(apiError.errorCode).json({
+        data: apiError.data,
+      })
+    }
+  }
+}
