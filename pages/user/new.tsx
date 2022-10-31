d9eda3d5794f3fb002d87ca2d8f72a99c031bcaf
andresjesse <andres.porfirio@gmail.com>
2022-10-31 19:57:25 -0300

    rebase and better error console.log

--- pages/user/new.tsx
+++ pages/user/new.tsx
@@ -1,8 +1,4 @@
-import {
-  Button, Form,
-  Input, Select,
-  Typography
-} from 'antd'
+import { Button, Form, Input, Select, Typography } from 'antd'
 import axios from 'axios'
 import { NextPage } from 'next'
 import { useRouter } from 'next/router'
@@ -10,50 +6,52 @@ import { useState } from 'react'
 import { fakeCrmUf } from '../../services/fakeCrmUf'
 import newStyles from './new.module.css'
 const { Title } = Typography
-const dateFormat = ('DD/MM/YYYY')
+const dateFormat = 'DD/MM/YYYY'
+
+const initialValues = {
+  nameDoctor: '',
+  crm: '',
+  crmUf: '',
+  email: '',
+  confEmail: '',
+  password: '',
+  confPassword: '',
+}
 
 /**
  * FORMULARIO DE CADASTRO
  */
 const Login: NextPage = () => {
+  const [formValue, setValor] = useState(initialValues)
 
-  const initialValues = {
-    nameDoctor: '',
-    crm: '',
-    crmUf: '',
-    email: '',
-    confEmail: '',
-    password: '',
-    confPassword: '',
+  function handleChange(event: { target: { name: any; value: any } }) {
+    const { name, value } = event.target
+    setValor({ ...formValue, [name]: value })
   }
 
-  const [formValue, setValor] = useState(initialValues);
+  const router = useRouter()
 
-  function handleChange(event: { target: { name: any; value: any } }){
-    const {name, value } = event.target;
-    setValor({...formValue, [name]: value});
-  }
-  
-  const router = useRouter();
-
-
-  function handleSubmit(req: any){
+  function handleSubmit(req: any) {
     axios
-    .post("/api/doctor/create", req)
-    .then((resp) => {router.push("/");})
-    .catch((e) => {console.log(e, "   deu errado");});
-  } 
+      .post('/api/doctor/create', req)
+      .then((resp) => {
+        router.push('/')
+      })
+      .catch((e) => {
+        console.log(e, e.response.data.data)
+      })
+  }
 
-  function validete(){
-    console.log("validando...")
-    
+  function validete() {
+    console.log('validando...')
+
     const aux = {
-      name: formValue.nameDoctor, 
+      name: formValue.nameDoctor,
       email: formValue.email,
       password: formValue.password,
       crm: formValue.crm,
       crmUf: formValue.crmUf,
-    };
+    }
 
     /*console.log("validando email...")
     if(formValue.email == formValue.confEmail){
@@ -62,78 +60,107 @@ const Login: NextPage = () => {
       console.log("email errado...")
     }*/
 
-    handleSubmit(aux);
+    handleSubmit(aux)
   }
 
+  return (
+    <div className={newStyles.authPageWrapper}>
+      <div className={newStyles.formContainer}>
+        <Form
+          layout="horizontal"
+          labelWrap
+          labelCol={{ flex: '120px' }}
+          wrapperCol={{ flex: 1 }}
+          colon={false}
+          style={{ width: '100%' }}
+          onFinish={validete}
+        >
+          <Title className={newStyles.textCenter} level={3}>
+            Cadastro Novo Usuario
+          </Title>
 
+          <Form.Item label="Nome: ">
+            <Input id="nameDoctor" name="nameDoctor" onChange={handleChange} />
+          </Form.Item>
 
-  
-    return (
-      <div className={newStyles.authPageWrapper}>
-        <div className={newStyles.formContainer}>
-          <Form
-              layout="horizontal"
-              labelWrap
-              labelCol={{ flex: '120px' }}
-              wrapperCol={{ flex: 1 }}
-              colon={false}
-              style={{ width: '100%' }}
-              onFinish={validete}
-            >
-            <Title className={newStyles.textCenter} level={3}>Cadastro Novo Usuario</Title>
-  
-              <Form.Item label="Nome: ">
-                <Input id="nameDoctor" name="nameDoctor" onChange={handleChange}/>
-              </Form.Item>
-
-              {/*DATA DE NASCIMENTO DESATIVADA PARA FUTURA IMPLANTAÇÃO*/}
-              {/*<Form.Item label="Data de Nascimento: ">
+          {/*DATA DE NASCIMENTO DESATIVADA PARA FUTURA IMPLANTAÇÃO*/}
+          {/*<Form.Item label="Data de Nascimento: ">
                 <DatePicker onChange={(date) => {
                   setValor({...formValue, ['birthday']: moment(date).format(dateFormat)});
                 }} id="birthday" name="birthday" className={newStyles.formBorderRadius} format={dateFormat} />
               </Form.Item>*/}
-  
-              <Form.Item label="CRM: ">
-                <Input id="crm" name="crm" onChange={handleChange}/>
-              </Form.Item>
-  
-              <Form.Item name="crmUf" label="CRM UF: ">
-                <Select onChange={(uf) =>{
-                  setValor({...formValue, ['crmUf']: uf});
-                }} id="crmUf" className={newStyles.formBorderRadius}>
-                  {fakeCrmUf.map((uf) => (
-                    <Select.Option key={uf} value={uf}>
-                      {uf}
-                    </Select.Option>
-                  ))}
-                </Select>
-              </Form.Item>
-  
-              <Form.Item label="Email: ">
-                <Input id="email" name="email" alt="Seu E-mail" onChange={handleChange}/>
-              </Form.Item>
-  
-              <Form.Item label="Confirme o Email: ">
-                <Input id="confEmail" name="confEmail" alt="Confirme seu E-mail" onChange={handleChange}/>
-              </Form.Item>
-  
-              <Form.Item label="Senha: ">
-                <Input.Password id="password" name="password" alt="Senha" onChange={handleChange}/>
-              </Form.Item>
-  
-              <Form.Item label="Confirme sua Senha: ">
-                <Input.Password id="confPassword" name="confPassword" alt="Confirme a Senha" onChange={handleChange}/>
-              </Form.Item>
-  
-              <Form.Item label=" ">
-                <Button className={newStyles.button} shape="round" size="large" type="primary" htmlType="submit">
-                  Cadastrar
-                </Button>
-              </Form.Item>
-            </Form>
-        </div>
+
+          <Form.Item label="CRM: ">
+            <Input id="crm" name="crm" onChange={handleChange} />
+          </Form.Item>
+
+          <Form.Item name="crmUf" label="CRM UF: ">
+            <Select
+              onChange={(uf) => {
+                setValor({ ...formValue, ['crmUf']: uf })
+              }}
+              id="crmUf"
+              className={newStyles.formBorderRadius}
+            >
+              {fakeCrmUf.map((uf) => (
+                <Select.Option key={uf} value={uf}>
+                  {uf}
+                </Select.Option>
+              ))}
+            </Select>
+          </Form.Item>
+
+          <Form.Item label="Email: ">
+            <Input
+              id="email"
+              name="email"
+              alt="Seu E-mail"
+              onChange={handleChange}
+            />
+          </Form.Item>
+
+          <Form.Item label="Confirme o Email: ">
+            <Input
+              id="confEmail"
+              name="confEmail"
+              alt="Confirme seu E-mail"
+              onChange={handleChange}
+            />
+          </Form.Item>
+
+          <Form.Item label="Senha: ">
+            <Input.Password
+              id="password"
+              name="password"
+              alt="Senha"
+              onChange={handleChange}
+            />
+          </Form.Item>
+
+          <Form.Item label="Confirme sua Senha: ">
+            <Input.Password
+              id="confPassword"
+              name="confPassword"
+              alt="Confirme a Senha"
+              onChange={handleChange}
+            />
+          </Form.Item>
+
+          <Form.Item label=" ">
+            <Button
+              className={newStyles.button}
+              shape="round"
+              size="large"
+              type="primary"
+              htmlType="submit"
+            >
+              Cadastrar
+            </Button>
+          </Form.Item>
+        </Form>
       </div>
-    );  
+    </div>
+  )
 }
 
-export default Login;
+export default Login
