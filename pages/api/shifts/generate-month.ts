9bc654785edd99a936ec33154ab347788903d733
Andres <andres.porfirio@gmail.com>
2022-11-07 19:47:06 -0300

    fixed generation alg

--- pages/api/shifts/generate-month.ts
+++ pages/api/shifts/generate-month.ts
@@ -2,6 +2,7 @@ import { NextApiRequest, NextApiResponse } from 'next'
 import { prisma } from '../../../lib/prisma'
 import formatHashShift from '../../../services/formatHashShift'
 import { ApiHandleError } from '../../../utils/api/apiHandleError'
+import withErrorHandler from '../../../utils/api/withErrorHandler'
 
 // helper function
 const createDateUTC = (baseDate: Date, utcHour: number) => {
@@ -15,90 +16,94 @@ const createDateUTC = (baseDate: Date, utcHour: number) => {
   )
 }
 
-export default async function handler(
+async function handlerFunction(
   req: NextApiRequest,
   res: NextApiResponse<string | null>
 ) {
   if (req.method === 'POST') {
-    try {
-      const { month, year } = req.body
+    const { month, year } = req.body
 
-      //check params (you can also return a single ApiHandleError with an array of erros, check /api/doctor/create for details)
-      if (!month) throw new ApiHandleError(400, 'month is required!')
-      if (!year) throw new ApiHandleError(400, 'year is required!')
+    //check params (you can also return a single ApiHandleError with an array of erros, check /api/doctor/create for details)
+    if (typeof month !== 'number')
+      throw new ApiHandleError(400, 'month is required!')
+    if (month < 0 || month > 11)
+      throw new ApiHandleError(
+        400,
+        'month must be a number between 0 (january) and 11 (december)!'
+      )
+    if (typeof year !== 'number')
+      throw new ApiHandleError(400, 'year is required!')
 
-      const sectors = await prisma.sector.findMany()
+    const sectors = await prisma.sector.findMany()
 
-      const shiftsToGenerate: Array<{
-        startDate: Date
-        endDate: Date
-        isFixed: boolean
-        idSector: number
-        hash: string
-      }> = []
+    const shiftsToGenerate: Array<{
+      startDate: Date
+      endDate: Date
+      isFixed: boolean
+      idSector: number
+      hash: string
+    }> = []
 
-      // Define consts (for now shifts are fixed)
-      const SHIFTS = [
-        {
-          START_UTC: 10, // 7AM in localtime (America/Sao_Paulo)
-          END_UTC: 22, //7PM in localtime (America/Sao_Paulo)
-        },
-        {
-          START_UTC: 22, // 7PM in localtime (America/Sao_Paulo)
-          END_UTC: 22 + 12, //7AM (next day +12h) in localtime (America/Sao_Paulo)
-        },
-      ]
+    // Define consts (for now shifts are fixed)
+    const SHIFTS = [
+      {
+        START_UTC: 10, // 7AM in localtime (America/Sao_Paulo)
+        END_UTC: 22, //7PM in localtime (America/Sao_Paulo)
+      },
+      {
+        START_UTC: 22, // 7PM in localtime (America/Sao_Paulo)
+        END_UTC: 22 + 12, //7AM (next day +12h) in localtime (America/Sao_Paulo)
+      },
+    ]
 
-      const date = new Date(year, month, 1)
-      while (date.getMonth() === month) {
-        sectors.forEach((sector) => {
-          SHIFTS.forEach((shift) => {
-            const startDate = createDateUTC(date, shift.START_UTC)
-            const endDate = createDateUTC(date, shift.END_UTC)
+    const date = new Date(year, month, 1)
+    while (date.getMonth() === month) {
+      sectors.forEach((sector) => {
+        SHIFTS.forEach((shift) => {
+          const startDate = createDateUTC(date, shift.START_UTC)
+          const endDate = createDateUTC(date, shift.END_UTC)
 
-            shiftsToGenerate.push({
-              startDate,
-              endDate,
-              idSector: sector.id,
-              isFixed: false,
-              hash: formatHashShift(startDate, sector.id),
+          shiftsToGenerate.push({
+            startDate,
+            endDate,
+            idSector: sector.id,
+            isFixed: false,
+            hash: formatHashShift(startDate, sector.id),
+          })
+
+          console.log('generating: ', sector.abbreviation, startDate)
+
+          console.log(
+            '\t',
+            startDate.toLocaleString('pt-BR', {
+              timeZone: 'America/Sao_Paulo',
+            }),
+            endDate.toLocaleString('pt-BR', {
+              timeZone: 'America/Sao_Paulo',
             })
-
-            console.log('generating: ', sector.abbreviation, startDate)
-
-            console.log(
-              '\t',
-              startDate.toLocaleString('pt-BR', {
-                timeZone: 'America/Sao_Paulo',
-              }),
-              endDate.toLocaleString('pt-BR', {
-                timeZone: 'America/Sao_Paulo',
-              })
-            )
-          })
+          )
         })
+      })
 
-        date.setDate(date.getDate() + 1)
-      }
-
-      const generatedShifts = await prisma.$transaction([
-        ...shiftsToGenerate.map((shift) =>
-          prisma.shift.upsert({
-            where: {
-              hash: shift.hash,
-            },
-            create: {
-              ...shift,
-            },
-            update: {},
-          })
-        ),
-      ])
-
-      res.status(201).json(`generated ${generatedShifts.length} shifts!`)
-    } catch (error) {
-      console.log(error)
-      res.status(500).json(null)
+      date.setDate(date.getDate() + 1)
     }
+
+    const generatedShifts = await prisma.$transaction([
+      ...shiftsToGenerate.map((shift) =>
+        prisma.shift.upsert({
+          where: {
+            hash: shift.hash,
+          },
+          create: {
+            ...shift,
+          },
+          update: {},
+        })
+      ),
+    ])
+
+    res.status(201).json(`generated ${generatedShifts.length} shifts!`)
   }
 }
+
+export default withErrorHandler(handlerFunction)
