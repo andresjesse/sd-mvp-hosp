320dadf3ebd818b3fd4a14856ee623ddf8dc6bef
guilhermeferreira7 <guilhermefribas@outlook.com>
2022-11-08 14:38:04 -0300

    letting the admin change the date to generate schedules

--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -1,23 +1,35 @@
-import { Button, Card, DatePicker, Form } from 'antd'
+import { Button, Card, DatePicker, DatePickerProps, Form } from 'antd'
+import axiosApi from '../../services/axiosApi'
 import styles from './styles.module.css'
 
-export default function admin() {
+export default function Admin() {
+  const date: {
+    month?: number
+    year?: number
+  } = {
+    month: 0,
+    year: 0,
+  }
+
+  const onChange: DatePickerProps['onChange'] = (moment) => {
+    date.month = moment?.month()
+    date.year = moment?.year()
+  }
+
   const generateSchedules = async () => {
-    // const month = new Date().getMonth()
-    // const year = new Date().getFullYear()
+    const month = date.month
+    const year = date.year
 
     try {
-      console.log('works!!')
+      const response = await axiosApi.post('/api/shifts/generate-month', {
+        month,
+        year,
+      })
 
-      //   const response = await axiosApi.post('/api/shifts/generate-month', {
-      //     month,
-      //     year,
-      //   })
-
-      //   const { status, data } = response
+      const { status, data } = response
 
       //   //TODO: show user feedback
-      //   console.log(status, data)
+      console.log(status, data)
     } catch (error) {
       //TODO: show user feedback (e.g. https://ant.design/components/notification/ )
     }
@@ -28,7 +40,7 @@ export default function admin() {
       <Card title="Gerar escalas do mês">
         <Form>
           <Form.Item name="month" hasFeedback label="Mês">
-            <DatePicker picker="month" />
+            <DatePicker picker="month" onChange={onChange} />
           </Form.Item>
           <Form.Item className={styles.buttonSchedulesGenerate}>
             <Button onClick={generateSchedules} shape="round">
