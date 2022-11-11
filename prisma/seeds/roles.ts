b269d7f008cab7e74b0c6b1063a0bec94a73f6f3
andresjesse <andres.porfirio@gmail.com>
2022-11-11 14:47:56 -0300

    updated user roles. changed admin seeder

---
+++ prisma/seeds/roles.ts
@@ -0,0 +1,12 @@
+import Roles from '../../utils/auth/Roles'
+
+export const roles = [
+  {
+    id: Roles.ADMIN,
+    title: 'ADMIN',
+  },
+  {
+    id: Roles.DOCTOR,
+    title: 'DOCTOR',
+  },
+]
---
+++ utils/auth/Roles.ts
@@ -0,0 +1,6 @@
+enum Roles {
+  ADMIN = 1,
+  DOCTOR = 2,
+}
+
+export default Roles
