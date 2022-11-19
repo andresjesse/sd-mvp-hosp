a94a93bba611163ad55605508d36d08d37c5073a
alguipires <alguipires@gmail.com>
2022-11-19 19:33:52 -0300

    pagina de cadastro com rules validação implementado

--- pages/user/new.tsx
+++ pages/user/new.tsx
@@ -3,18 +3,15 @@ import router from 'next/router'
 import { useState } from 'react'
 import axiosApi from '../../services/axiosApi'
 import { fakeCrmUf } from '../../services/fakeCrmUf'
-const { Title } = Typography
-
 import styles from './styles.module.css'
+const { Title, Text } = Typography
 
 const initialValues = {
-  nameDoctor: '',
+  name: '',
   crm: '',
   crmUf: '',
   email: '',
-  confEmail: '',
   password: '',
-  confPassword: '',
 }
 
 const initialObjError = {
@@ -29,127 +26,48 @@ const New: React.FC = () => {
   const [formValue, setValor] = useState(initialValues)
   const [objError, setObjError] = useState(initialObjError)
 
-  function handleChange(event: { target: { name: string; value: string } }) {
-    const { name, value } = event.target
-    setValor({ ...formValue, [name]: value })
+  function handleChange(event: { target: { id: string; value: string } }) {
+    const { id, value } = event.target
+    setValor({ ...formValue, [id]: value })    
   }
 
   function handleSubmit(req: object) {
     axiosApi
       .post('/api/doctor/create', req)
-      //.then((response) => setObjError(response.data))
       .then((response) => router.push('/welcome'))
       .catch((e) => {
-        //console.log(e, e.response.data.data)
-        setObjError(e.response.data.data)
-
-        //TENTATIVA DE PEGAR O NAME DO ERRO E COMPARAR COM O OBJETO STATUSERRO E INTERAGIR COM HTML CONFORME O ERRO.
-        /*if(e.response.data.data != null){
-          let aux = e.response.data.data;
-          console.log("deu erro....", );
-        }*/
+        if(e.response.data.data){
+          setObjError(e.response.data.data)
+          return
+        }                
       })
-  }
-
-  //FAZER VALIDAÇÃO ANTES DE MANDAR PARA SERVIDOR.
-  function validete() {
-    console.log('validando...')
-
-    let aux = {
-      name: formValue.nameDoctor,
-      email: formValue.email,
-      password: formValue.password,
-      crm: formValue.crm,
-      crmUf: formValue.crmUf,
-    }
-
-    /*
-    let aux = {
-      name: '',
-      email: '',
-      password: '',
-      crm: '',
-      crmUf: '',
-    }
-
-    let counterros = false;
-
-    if(!formValue.nameDoctor){
-      setObjError({...objError, name: 'Nome não pode ser vazio!'})
-    }else{
-      setObjError({...objError, name: ''})
-      aux.name = formValue.nameDoctor
-    }
-
-    //verifcar esta passando com valor null
-    if(!formValue.crm){
-      setObjError({...objError, crm: 'CRM não pode ser vazio!'})
-    }else{
-      setObjError({...objError, crm: ''})
-      aux.crm = formValue.crm
-    }
-
-    if(!formValue.crmUf){
-      setObjError({...objError, crmUf: 'CRM UF não pode ser vazio!'})
-    }else{
-      setObjError({...objError, crmUf: ''})
-      aux.crmUf = formValue.crmUf
-    }
-
-    if(formValue.email != formValue.confEmail){
-      setObjError({...objError, email: 'Email não são iguais!'})
-    }else{
-      setObjError({...objError, email: ''})
-    }
-
-    if(!formValue.email){
-      setObjError({...objError, email: 'Email não pode ser vazio!'})
-    }else{
-      setObjError({...objError, email: ''})
-      aux.email = formValue.email
-    }
-
-    if(formValue.password != formValue.confPassword){
-      setObjError({...objError, password: 'Senha não são iguais!'})
-    }else{
-      setObjError({...objError, password: ''})
-    }
-
-    if(!formValue.password){
-      setObjError({...objError, password: 'Senha não pode ser vazio!'})
-    }else{
-      setObjError({...objError, password: ''})
-      aux.password = formValue.password
-    }*/
-
-    console.log('validado!')
-    handleSubmit(aux)
   }
 
   return (
     <div className={styles.authPageWrapper}>
       <div className={styles.formContainer}>
         <Form
+          name="formNew"
           layout="horizontal"
           labelWrap
           labelCol={{ flex: '120px' }}
           wrapperCol={{ flex: 1 }}
           colon={false}
           style={{ width: '100%' }}
-          onFinish={validete}
+          onFinish={()=>{handleSubmit(formValue)}}
+          scrollToFirstError
         >
           <Title className={styles.textCenter} level={3}>
             Cadastro Novo Usuario
           </Title>
 
-          <Form.Item validateStatus="validating" label="Nome: ">
-            <Input
-              id="nameDoctor"
-              value={formValue?.nameDoctor}
-              name="nameDoctor"
-              onChange={handleChange}
-            />
-            <label style={{ color: 'red' }}>{objError?.name}</label>
+          <Form.Item
+            name="name"
+            label="Nome: "    
+            hasFeedback
+             rules={[{ required: true, message: 'Por favor insira seu Nome!',  whitespace: true }]}
+            >
+            <Input id="name" onChange={handleChange} />
           </Form.Item>
 
           {/*DATA DE NASCIMENTO DESATIVADA PARA FUTURA IMPLANTAÇÃO*/}
@@ -159,17 +77,25 @@ const New: React.FC = () => {
                 }} id="birthday" name="birthday" className={styles.formBorderRadius} format={dateFormat} />
               </Form.Item>*/}
 
-          <Form.Item label="CRM: ">
-            <Input id="crm" name="crm" onChange={handleChange} />
-            <label style={{ color: 'red' }}>{objError?.crm}</label>
+          <Form.Item
+            name="crm"
+            label="CRM: "
+            hasFeedback
+            rules={[{ required: true, message: 'Por favor insira seu CRM!' }]}
+            >
+            <Input id="crm" onChange={handleChange} />
           </Form.Item>
 
-          <Form.Item name="crmUf" label="CRM UF: ">
+          <Form.Item
+            name="crmUf"
+            label="CRM UF: "
+            hasFeedback
+            rules={[{ required: true, message: 'Por favor insira seu CRM-UF!' }]}
+            >
             <Select
               onChange={(uf) => {
                 setValor({ ...formValue, ['crmUf']: uf })
               }}
-              id="crmUf"
               className={styles.formBorderRadius}
             >
               {fakeCrmUf.map((uf) => (
@@ -178,67 +104,89 @@ const New: React.FC = () => {
                 </Select.Option>
               ))}
             </Select>
-            <label style={{ color: 'red' }}>{objError?.crmUf}</label>
           </Form.Item>
 
-          <Form.Item label="Email: ">
-            <Input
-              id="email"
-              name="email"
-              alt="Seu E-mail"
-              onChange={handleChange}
-            />
-            <label style={{ color: 'red' }}>{objError?.email}</label>
+          <Form.Item 
+            name="email"
+            label="Email: "
+            hasFeedback
+            rules={[
+              {
+                required: true,
+                message: 'Por favor insira seu e-mail.',
+              },
+              {
+                type: 'email',
+                message: 'E-mail invalido.',
+              },
+            ]}
+            >
+            <Input id="email" onChange={handleChange} />
           </Form.Item>
 
           <Form.Item
             label="Confirme o Email: "
-            // dependencies={['email']}
-            // rules={[
-            //   {
-            //     required: true,
-            //     message: 'Please confirm your email!',
-            //   },
-            //   ({ getFieldValue }) => ({
-            //     validator(_, value) {
-            //       if (!value || getFieldValue('email') === value) {
-            //         return Promise.resolve()
-            //       }
-            //       return Promise.reject(
-            //         new Error('The two emails that you entered do not match!')
-            //       )
-            //     },
-            //   }),
-            // ]}
+            name="confEmail"
+            dependencies={['email']}
+            hasFeedback
+            rules={[
+               {
+                 required: true,
+                 message: 'Por favor confirme seu E-mail!',
+               },
+               ({ getFieldValue }) => ({
+                 validator(_, value) {
+                   if (!value || getFieldValue('email') === value) {
+                     return Promise.resolve()
+                   }
+                   return Promise.reject(
+                     new Error('Os E-mail não são iguais!')
+                   )
+                 },
+               }),
+             ]}
           >
-            <Input
-              id="confEmail"
-              name="confEmail"
-              alt="Confirme seu E-mail"
-              onChange={handleChange}
-            />
-            <label style={{ color: 'red' }}>{objError?.email}</label>
+            <Input/>
           </Form.Item>
 
-          <Form.Item label="Senha: ">
-            <Input.Password
-              id="password"
-              name="password"
-              alt="Senha"
-              onChange={handleChange}
-            />
-            <label style={{ color: 'red' }}>{objError?.password}</label>
+          <Form.Item
+            name="password"
+            label="Senha: "
+            rules={[
+              {
+                required: true,
+                message: 'Por favor insira sua senha.',
+              },
+            ]}
+            hasFeedback
+            >
+            <Input.Password id="password" onChange={handleChange}/>
           </Form.Item>
 
-          <Form.Item label="Confirme sua Senha: ">
-            <Input.Password
-              id="confPassword"
-              name="confPassword"
-              alt="Confirme a Senha"
-              onChange={handleChange}
-            />
-            <label style={{ color: 'red' }}>{objError?.password}</label>
+          <Form.Item 
+            name="confPassword"
+            label="Confirme sua Senha: "
+            dependencies={['password']}
+            rules={[
+              {
+                required: true,
+                message: 'Por favor confirme sua senha!',
+              },
+              ({ getFieldValue }) => ({
+                validator(_, value) {
+                  if (!value || getFieldValue('password') === value) {
+                    return Promise.resolve();
+                  }
+                  return Promise.reject(new Error('As senhas não são iguais!'));
+                },
+              }),
+            ]}
+            hasFeedback
+            >
+            <Input.Password/>
           </Form.Item>
+
+          {objError && <Text type="danger">{objError.name || objError.crm || objError.crmUf || objError.email || objError.password}</Text>}
 
           <Form.Item label=" ">
             <Button
