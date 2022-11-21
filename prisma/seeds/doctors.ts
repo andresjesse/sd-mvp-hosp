e0ca2c663204ea8df364ab39527d2c3f8f391e55
Guilherme Ferreira <54293930+guilhermeferreira7@users.noreply.github.com>
2022-11-21 15:58:10 -0300

    seed doctors (#21)
    * seed doctors
    
    * renamed function and seed doctor type according to admin seed
    
    Co-authored-by: Andres <andres.porfirio@gmail.com>

---
+++ prisma/seeds/doctors.ts
@@ -0,0 +1,34 @@
+import hasher from '../../utils/hasher/BcryptjsHasher'
+
+type TDoctorUser = {
+  name: string
+  email: string
+  passwordHash: string
+  doctor: {
+    create: {
+      crmUf: string
+      crm: string
+    }
+  }
+}
+
+export default async function generateDoctorsSeed(length: number) {
+  const doctors: Array<TDoctorUser> = []
+  const passwordHash = await hasher.hashAsync('123456')
+
+  for (let i = 1; i <= length; i++) {
+    doctors.push({
+      name: 'Doctor Example',
+      email: `doctor${i}@example.com`,
+      passwordHash,
+      doctor: {
+        create: {
+          crm: `123123${i}`,
+          crmUf: 'PR',
+        },
+      },
+    })
+  }
+
+  return doctors
+}
