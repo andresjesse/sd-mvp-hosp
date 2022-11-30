201072b949faa77131a0bfc66626955f43bb11e9
Marcos Rocha <marcosviniciusrochadasilva@gmail.com>
2022-11-30 19:32:32 -0300

    Added connection to Prisma

--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -1,233 +1,116 @@
-import { Button, Form, Input, notification, Select, Typography } from 'antd'
+import { Form, Input, Button, Select, Typography } from 'antd'
 import router from 'next/router'
 import { useState } from 'react'
 import axiosApi from '../../services/axiosApi'
+import Link from 'next/link'
 import { fakeCrmUf } from '../../services/fakeCrmUf'
 import styles from './styles.module.css'
 const { Title, Text } = Typography
+import { GetServerSideProps } from 'next'
+import { SelectOutlined } from '@ant-design/icons'
+import { prisma } from '../../lib/prisma'
 
-const initialValues = {
-  name: '',
-  crm: '',
-  crmUf: '',
-  email: '',
-  password: '',
+type ProfileSummary = {
+  name: String
+  crm: String
+  crmUf: String
+  email: String
 }
 
-const initialObjError = {
-  name: '',
-  email: '',
-  password: '',
-  crm: '',
-  crmUf: '',
+interface ProfilePageProps {
+  data: ProfileSummary
 }
 
-const Profile: React.FC = () => {
-  const [formValue, setValor] = useState(initialValues)
-  const [objError, setObjError] = useState(initialObjError)
-
-  function handleChange(event: { target: { id: string; value: string } }) {
-    const { id, value } = event.target
-    setValor({ ...formValue, [id]: value })
-  }
-
-  function handleSubmit(req: object) {
-    axiosApi
-      .post('/api/doctor/create', req)
-      .then((response) => {
-        if (response.status == 201) {
-          notification.open({
-            message: 'Usuário criado com sucesso!',
-            description: 'Você pode fazer login agora.',
-            duration: 0,
-          })
-          router.push('/login')
-        } else {
-          notification.open({
-            message: 'Erro ao criar usuário!',
-            description: 'Entre em contato com o suporte.',
-            duration: 0,
-          })
-        }
-      })
-      .catch((e) => {
-        if (e.response.data.data) {
-          setObjError(e.response.data.data)
-          return
-        }
-      })
-  }
-
+export default function ProfilePage({ data }: ProfilePageProps) {
   return (
     <div className={styles.authPageWrapper}>
       <div className={styles.formContainer}>
         <Form
           name="formNew"
-          layout="horizontal"
+          //layout="horizontal"
           disabled={true}
-          labelWrap
-          labelCol={{ flex: '120px' }}
-          // wrapperCol={{ flex: 1 }}
-          colon={false}
-          style={{ width: '100%' }}
-          onFinish={() => {
-            handleSubmit(formValue)
-          }}
-          scrollToFirstError
         >
-          <Title className={styles.textCenter} level={3}>
+          <Title className={styles.textCenter} level={2}>
             Meu Perfil
           </Title>
 
           <Form.Item
             name="name"
             label="Nome: "
-            hasFeedback
-            rules={[
-              {
-                required: true,
-                message: 'Por favor insira seu Nome!',
-                whitespace: true,
-              },
-            ]}
+            initialValue={data.name}
+            labelCol={{ span: 24 }}
+            wrapperCol={{ span: 24 }}
           >
-            <Input id="name" onChange={handleChange} />
+            <Input id="name" size="large" />
           </Form.Item>
-
           <Form.Item
             name="crm"
             label="CRM: "
-            hasFeedback
-            rules={[{ required: true, message: 'Por favor insira seu CRM!' }]}
+            initialValue={data.crm}
+            labelCol={{ span: 24 }}
+            wrapperCol={{ span: 24 }}
           >
-            <Input id="crm" onChange={handleChange} />
+            <Input id="crm" size="large" />
           </Form.Item>
-
           <Form.Item
             name="crmUf"
             label="CRM UF: "
-            hasFeedback
-            rules={[
-              { required: true, message: 'Por favor insira seu CRM-UF!' },
-            ]}
+            initialValue={data.crmUf}
+            labelCol={{ span: 24 }}
+            wrapperCol={{ span: 24 }}
           >
-            <Select
-              onChange={(uf) => {
-                setValor({ ...formValue, ['crmUf']: uf })
-              }}
-              className={styles.formBorderRadius}
-            >
-              {fakeCrmUf.map((uf) => (
-                <Select.Option key={uf} value={uf}>
-                  {uf}
-                </Select.Option>
-              ))}
-            </Select>
+            <Input id="crmUf" size="large" />
           </Form.Item>
-
           <Form.Item
             name="email"
             label="Email: "
-            hasFeedback
-            rules={[
-              {
-                required: true,
-                message: 'Por favor insira seu e-mail.',
-              },
-              {
-                type: 'email',
-                message: 'E-mail invalido.',
-              },
-            ]}
+            initialValue={data.email}
+            labelCol={{ span: 24 }}
+            wrapperCol={{ span: 24 }}
           >
-            <Input id="email" onChange={handleChange} />
+            <Input id="email" size="large" />
           </Form.Item>
-
-          <Form.Item
-            label="Confirme o Email: "
-            name="confEmail"
-            dependencies={['email']}
-            hasFeedback
-            rules={[
-              {
-                required: true,
-                message: 'Por favor confirme seu E-mail!',
-              },
-              ({ getFieldValue }) => ({
-                validator(_, value) {
-                  if (!value || getFieldValue('email') === value) {
-                    return Promise.resolve()
-                  }
-                  return Promise.reject(new Error('Os E-mail não são iguais!'))
-                },
-              }),
-            ]}
+          <Button
+            className={styles.button}
+            shape="round"
+            icon={<SelectOutlined />}
+            disabled={false}
+            size="large"
           >
-            <Input />
-          </Form.Item>
-
-          <Form.Item
-            name="password"
-            label="Senha: "
-            hasFeedback
-            rules={[
-              {
-                required: true,
-                message: 'Por favor insira sua senha.',
-              },
-            ]}
-          >
-            <Input.Password id="password" onChange={handleChange} />
-          </Form.Item>
-
-          <Form.Item
-            name="confPassword"
-            label="Confirme sua Senha: "
-            dependencies={['password']}
-            hasFeedback
-            rules={[
-              {
-                required: true,
-                message: 'Por favor confirme sua senha!',
-              },
-              ({ getFieldValue }) => ({
-                validator(_, value) {
-                  if (!value || getFieldValue('password') === value) {
-                    return Promise.resolve()
-                  }
-                  return Promise.reject(new Error('As senhas não são iguais!'))
-                },
-              }),
-            ]}
-          >
-            <Input.Password />
-          </Form.Item>
-
-          {objError && (
-            <Text type="danger">
-              {objError.name ||
-                objError.crm ||
-                objError.crmUf ||
-                objError.email ||
-                objError.password}
-            </Text>
-          )}
-
-          <Form.Item label=" ">
-            <Button
-              className={styles.button}
-              shape="round"
-              size="large"
-              type="primary"
-              htmlType="submit"
-            >
-              Cadastrar
-            </Button>
-          </Form.Item>
+            <Link href="profile/edit"> Editar Perfil</Link>
+          </Button>
         </Form>
       </div>
     </div>
   )
 }
 
-export default Profile
+export const getServerSideProps: GetServerSideProps = async () => {
+  const id = 2
+
+  const selectedUser = await prisma.user.findUnique({
+    where: {
+      id: id,
+    },
+  })
+
+  const selectedDoctor = await prisma.doctor.findUnique({
+    where: {
+      id: id,
+    },
+  })
+
+  console.log(selectedUser?.name)
+
+  let profile: ProfileSummary = {
+    //Como resolver isso?
+    name: selectedUser?.name,
+    crm: selectedDoctor?.crm,
+    crmUf: selectedDoctor?.crmUf,
+    email: selectedUser?.email,
+  }
+
+  return {
+    props: { data: profile },
+  }
+}
