49af7a224eb71fcb947f472ab70671a433ae4c7d
MateusVinicio <mateusvinicio1@gmail.com>
2022-12-06 00:31:58 -0300

    add filter name and crm

--- components/Admin/DoctorActivationCard/index.tsx
+++ components/Admin/DoctorActivationCard/index.tsx
@@ -1,8 +1,11 @@
 import { Doctor, User } from '@prisma/client'
-import { Card, notification, Switch, Table } from 'antd'
-import { ColumnsType } from 'antd/es/table'
-import React from 'react'
+import { Card, notification, Switch, Table, Button, Input, Space } from 'antd'
+import { ColumnsType, ColumnType } from 'antd/es/table'
+import React, { useRef, useState } from 'react'
 import axiosApi from '../../../services/axiosApi'
+import { SearchOutlined } from '@ant-design/icons'
+import type { InputRef } from 'antd'
+import type { FilterConfirmProps } from 'antd/es/table/interface'
 
 // Activation Callback
 const toggleDoctorActivation = async (active: boolean, id: React.Key) => {
@@ -32,43 +35,11 @@ const toggleDoctorActivation = async (active: boolean, id: React.Key) => {
 type DataType = {
   key: React.Key
   name: string
-  email: string
   crm: string
+  email: string
   uf: string
   active: boolean
 }
-
-// Table Helper
-const columns: ColumnsType<DataType> = [
-  {
-    title: 'Nome',
-    dataIndex: 'name',
-  },
-  {
-    title: 'Email',
-    dataIndex: 'email',
-  },
-  {
-    title: 'CRM',
-    dataIndex: 'crm',
-  },
-  {
-    title: 'UF',
-    dataIndex: 'uf',
-  },
-  {
-    title: 'Ativo',
-    dataIndex: 'active',
-    render: (_, record) => (
-      <Switch
-        defaultChecked={record.active}
-        onClick={(checked) => {
-          toggleDoctorActivation(checked, record.key)
-        }}
-      />
-    ),
-  },
-]
 
 // Table Helper
 const getDataTable = (
@@ -78,8 +49,8 @@ const getDataTable = (
     return {
       key: item.id,
       name: item.user.name,
-      email: item.user.email,
       crm: item.crm,
+      email: item.user.email,
       uf: item.crmUf,
       active: item.isActive,
     }
@@ -91,10 +62,127 @@ interface DoctorActivationCardProps {
   doctors: Array<Doctor & { user: User }>
 }
 
+type DataIndex = keyof DataType
+
 export default function DoctorActivationCard({
   doctors,
 }: DoctorActivationCardProps) {
-  console.log(doctors)
+  const [searchText, setSearchText] = useState('')
+  const [searchedColumn, setSearchedColumn] = useState('')
+  const searchInput = useRef<InputRef>(null)
+
+  const handleSearch = (
+    selectedKeys: string[],
+    confirm: (param?: FilterConfirmProps) => void,
+    dataIndex: DataIndex
+  ) => {
+    confirm()
+    setSearchText(selectedKeys[0])
+    setSearchedColumn(dataIndex)
+  }
+
+  const handleReset = (clearFilters: () => void) => {
+    clearFilters()
+    setSearchText('')
+  }
+
+  const getColumnSearchProps = (
+    dataIndex: DataIndex
+  ): ColumnType<DataType> => ({
+    filterDropdown: ({
+      setSelectedKeys,
+      selectedKeys,
+      confirm,
+      clearFilters,
+    }) => (
+      <div style={{ padding: 8 }} onKeyDown={(e) => e.stopPropagation()}>
+        <Input
+          ref={searchInput}
+          placeholder={`Buscar ${dataIndex}`}
+          value={selectedKeys[0]}
+          onChange={(e) =>
+            setSelectedKeys(e.target.value ? [e.target.value] : [])
+          }
+          onPressEnter={() =>
+            handleSearch(selectedKeys as string[], confirm, dataIndex)
+          }
+          style={{ marginBottom: 8, display: 'block' }}
+        />
+        <Space>
+          <Button
+            type="primary"
+            onClick={() =>
+              handleSearch(selectedKeys as string[], confirm, dataIndex)
+            }
+            icon={<SearchOutlined />}
+            size="small"
+            style={{ width: 90 }}
+          >
+            Buscar
+          </Button>
+          <Button
+            onClick={() => {
+              clearFilters && handleReset(clearFilters)
+              confirm({ closeDropdown: false })
+            }}
+            size="small"
+            style={{ width: 90 }}
+          >
+            Limpar
+          </Button>
+        </Space>
+      </div>
+    ),
+    filterIcon: (filtered: boolean) => (
+      <SearchOutlined style={{ color: filtered ? '#1890ff' : undefined }} />
+    ),
+    onFilter: (value, record) =>
+      record[dataIndex]
+        .toString()
+        .toLowerCase()
+        .includes((value as string).toLowerCase()),
+    onFilterDropdownOpenChange: (visible) => {
+      if (visible) {
+        setTimeout(() => searchInput.current?.select(), 100)
+      }
+    },
+    render: (text) => text,
+  })
+
+  // Table Helper
+  const columns: ColumnsType<DataType> = [
+    {
+      title: 'Nome',
+      dataIndex: 'name',
+      ...getColumnSearchProps('name'),
+    },
+    {
+      title: 'CRM',
+      dataIndex: 'crm',
+      ...getColumnSearchProps('crm'),
+    },
+    {
+      title: 'Email',
+      dataIndex: 'email',
+    },
+    {
+      title: 'UF',
+      dataIndex: 'uf',
+    },
+    {
+      title: 'Ativo',
+      dataIndex: 'active',
+      render: (_, record) => (
+        <Switch
+          defaultChecked={record.active}
+          onClick={(checked) => {
+            toggleDoctorActivation(checked, record.key)
+          }}
+        />
+      ),
+    },
+  ]
+
   return (
     <Card title="Ativação de cadastro">
       <Table columns={columns} dataSource={getDataTable(doctors)} />
